01bd4ef427e7d5a566cf2d9d0a3f6b54fd9395fc
JohnJeong123 <frenchofkiss@gmail.com>
2017-03-08 16:38:25 +0900

    fixed Comment

--- PopbillFax.pas
+++ PopbillFax.pas
@@ -3,18 +3,7 @@
 * Unit for base module for Popbill FAX API SDK. Main functionality is to
 * send FAX with document file.( ex. pdf, doc, xls, hwp etc.)
 *
-* This module uses synapse library.( http://www.ararat.cz/synapse/doku.php/ )
-* It's full open source library, free to use include commercial application.
-* If you wish to donate that, visit their site.
-* So, before using this module, you need to install synapse by user self.
-* You can refer their site or detailed infomation about installation is available
-* from below our site. We appreciate your visiting.
-*
 * For strongly secured communications, this module uses SSL/TLS with OpenSSL.
-* So You need two dlls (libeay32.dll and ssleay32.dll) from OpenSSL. You can
-* get it from Fulgan. ( http://indy.fulgan.com/SSL/ ) We recommend i386_win32 version.
-* And also, dlls must be released with your executions. That's the drawback of this
-* module, but we acommplished higher security level against that.
 *
 * http://www.popbill.com
 * Author : Kim Seongjun (pallet027@gmail.com)
